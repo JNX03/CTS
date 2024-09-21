55de1b7f8894e4a8c9a6935c2eb890486cb42823
Jnx03 <jn03official@gmail.com>
2024-09-21 15:41:35 +0700

    Change map location

--- src/components/Map.js
+++ src/components/Map.js
@@ -41,7 +41,7 @@ function Map() {
         scrolling="no"
         marginHeight="0"
         marginWidth="0"
-        src="https://maps.google.com/maps?width=100%25&amp;height=100%25&amp;hl=en&amp;q=Google,%208th%20Avenue,%20New%20York,%20NY,%20USA&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1520.9373140618582!2d99.00587666534108!3d18.796884383161544!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30da3aac983453af%3A0xf7754b6645d9a9a2!2z4LmC4Lij4LiH4LmA4Lij4Li14Lii4LiZ4Lib4Lij4Li04LiZ4Liq4LmM4Lij4Lit4Lii4LmB4Lii4Lil4Liq4LmM4Lin4Li04LiX4Lii4Liy4Lil4Lix4Lii!5e0!3m2!1sth!2sth!4v1726907204910!5m2!1sth!2sth"
         title="Google Maps"
         style={styles.iframe}
       ></iframe>
